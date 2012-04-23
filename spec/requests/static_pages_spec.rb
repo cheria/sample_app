923abc23e9cff49500e7d0b219617eee5e5cf96b
cheria <chie.memo@gmail.com>
2012-04-24 01:33:40 +0900

    Finish Exercises Chapter5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,18 @@ describe "StaticPages" do
 
   subject{ page }
 
+  shared_examples_for "all static pages" do
+    it{ should have_selector('h1', text: heading) }
+    it{ should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading) {'Sample App'}
+    let(:page_title) {''}
 
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector 'title', text: full_title('') }
-    it { should_not have_selector 'title', text: '| Home'}
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text:'| Home' }
   end
 
   describe "Help page" do
@@ -31,5 +37,19 @@ describe "StaticPages" do
 
     it { should have_selector('h1', text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should #fill in
+    click_link "Contact"
+    page.should #fill in
+    click_link "Home"
+    page.should #fill in
+    click_link "Sign up now!"
+    page.should #fill in
   end
 end
